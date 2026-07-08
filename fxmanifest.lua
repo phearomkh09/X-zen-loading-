c312318cc857e5420fa87ee53c24b99350430377
X'zen-offlcial <khkha8410@gmail.com>
2026-07-09 06:23:39 +0700

    Add fxmanifest for X'zen Roleplay loading screen
    This file defines the fxmanifest for the X'zen Roleplay loading screen, including metadata and assets.

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "X'zen Roleplay"
+description "X'zen Roleplay Premium Loading Screen"
+version "1.0.0"
+
+loadscreen 'index.html'
+
+loadscreen_cursor 'yes'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+
+    'logo.png',
+
+    'video.mp4',
+
+    'music.mp3',
+    'music2.mp3',
+    'music3.mp3'
+}
